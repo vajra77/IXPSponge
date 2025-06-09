f3a93f6ae8a7b82b243ed3d4b6cdb7aa722bcb18
vajra77 <f.ferreri@namex.it>
2025-06-09 15:09:02 +0200

    added damonization features

--- xsponge.py
+++ xsponge.py
@@ -2,15 +2,42 @@ from scapy.layers.l2 import ARP, Ether
 from scapy.sendrecv import sendp, sniff
 from scapy.arch import get_if_hwaddr
 from time import sleep
+import daemon
+import signal
+import syslog
+import json
+import toml
 import getopt
 import sys
 
 
 SNIFF_CACHE = {}
 SPONGED_ADDRESSES = []
+RESERVED_ADDRESSES = []
+RUNNING = True
+
+
+def sig_shutdown(signum, frame):
+    global RUNNING
+    RUNNING = False
+    syslog.syslog(syslog.LOG_INFO, f"Shutdown signal received, saving sponge addresses to {CONFIG['backend']['running']}")
+    save_sponged_addresses(CONFIG['backend']['running'])
+    exit(0)
+
+
+def sig_reload(signum, frame):
+    global RUNNING
+    global SPONGED_ADDRESSES
+
+    RUNNING = False
+    syslog.syslog(syslog.LOG_INFO, f"Hangup signal received, reloading sponge addresses from {CONFIG['backend']['initial']}")
+    load_sponged_addresses(CONFIG['backend']['initial'])
+    RUNNING = True
 
 
 def arp_account(packet):
+    global SNIFF_CACHE
+
     if ARP in packet and packet[ARP].op == 1: #who-has or is-at
         src_addr = packet[ARP].psrc
         if src_addr in SNIFF_CACHE.keys():
@@ -19,9 +46,9 @@ def arp_account(packet):
             SNIFF_CACHE[src_addr] = 1
 
 
-def arp_sweep(interface, addresses, interval):
+def arp_sweep(interface, interval):
     src_mac = get_if_hwaddr(interface)
-    for ip_address in addresses:
+    for ip_address in SPONGED_ADDRESSES:
         reply = ARP(op=ARP.is_at, hwsrc=src_mac, psrc=ip_address, hwdst="ff:ff:ff:ff:ff:ff", pdst=ip_address)
         frame = Ether(dst="ff:ff:ff:ff:ff:ff", src=src_mac) / reply
         sendp(frame, iface=interface)
@@ -29,33 +56,123 @@ def arp_sweep(interface, addresses, interval):
 
 
 def arp_monitor(interface, count, threshold):
-        sniff(iface=interface, filter="arp", prn=arp_account, count=count)
-        for address in SNIFF_CACHE.keys():
-            if SNIFF_CACHE[address] > threshold:
-                add_sponged_addres(address)
-        SNIFF_CACHE.clear()
+    global SNIFF_CACHE
+
+    sniff(iface=interface, filter="arp", prn=arp_account, count=count)
+    for address in SNIFF_CACHE.keys():
+        if SNIFF_CACHE[address] > threshold:
+            if address in RESERVED_ADDRESSES:
+                syslog.syslog(syslog.LOG_WARNING, f"Attempt to sponge reserved address [{address}], skipping.")
+            else:
+                syslog.syslog(syslog.LOG_INFO,
+                              f"Adding [{address}] to sponge: {SNIFF_CACHE[address]}/{threshold}")
+                add_sponged_address(address)
+    SNIFF_CACHE.clear()
 
 
-def add_sponged_addres(address):
+def add_sponged_address(address):
+    global SPONGED_ADDRESSES
+
     if address not in SPONGED_ADDRESSES:
         SPONGED_ADDRESSES.append(address)
 
 
 def del_sponged_address(address):
+    global SPONGED_ADDRESSES
+
     if address in SPONGED_ADDRESSES:
         SPONGED_ADDRESSES.remove(address)
 
 
+def load_sponged_addresses(filename):
+    global SPONGED_ADDRESSES
+
+    SPONGED_ADDRESSES.clear()
+    with open(filename, 'r') as file:
+        data = json.load(file)
+        for address in data['addresses']:
+            if address in RESERVED_ADDRESSES:
+                syslog.syslog(syslog.LOG_WARNING, f"Attempt to load reserved address [{address}], skipping.")
+            else:
+                SPONGED_ADDRESSES.append(address)
+
+
+def load_reserved_addresses(filename):
+    global RESERVED_ADDRESSES
+
+    RESERVED_ADDRESSES.clear()
+    with open(filename, 'r') as file:
+        data = json.load(file)
+        RESERVED_ADDRESSES.extend(data['addresses'])
+
+
+def save_sponged_addresses(filename):
+    with open(filename, 'w') as f:
+        data = {'addresses': SPONGED_ADDRESSES}
+        json.dump(data, f) # noqa
+
+
+def usage():
+    print("Usage: xsponge -c <path_to_config_file>")
+
+
+def main():
+
+    global RUNNING
+
+    load_reserved_addresses(CONFIG['backend']['reserved'])
+
+    if CONFIG['sponge']['preload']:
+        load_sponged_addresses(CONFIG['backend']['initial'])
+
+    while RUNNING:
+        try:
+            if CONFIG['sponge']['enabled']:
+                arp_sweep(CONFIG['sponge']['interface'],
+                          CONFIG['sponge']['interval'])
+
+            if CONFIG['monitor']['enabled']:
+                arp_monitor(CONFIG['monitor']['interface'],
+                            CONFIG['monitor']['count'],
+                            CONFIG['monitor']['threshold'])
+                save_sponged_addresses(CONFIG['backend']['running'])
+
+            sleep(30)
+
+        except KeyboardInterrupt:
+            RUNNING = False
+
 
 if __name__ == "__main__":
 
-    interface = None
+    config_file = "xsponge.conf"
 
-    opts, args = getopt.getopt(sys.argv[1:], "i:", ["iface="])
+    opts, args = getopt.getopt(sys.argv[1:], "hc:", ["help", "config="])
 
     for opt, arg in opts:
-        if opt == "--iface":
-            interface = arg
+        if opt == "--config":
+            config_file = arg
+        elif opt == "--help":
+            usage()
+            exit(0)
         else:
-            assert False, "unhandled option"
+            print("Unhandled option %s" % opt, file=sys.stderr)
+            usage()
+            exit(1)
+
+    try:
+        CONFIG = toml.load(config_file, _dict=dict)
+    except FileNotFoundError:
+        print("Configuration file not found", file=sys.stderr)
+        usage()
+        exit(1)
+
+    with daemon.DaemonContext(
+            pidfile=CONFIG['pid_file'],
+            signal_map={
+                signal.SIGTERM: sig_shutdown,
+                signal.SIGTSTP: sig_shutdown,
+                signal.SIGHUP: sig_reload,
+            }):
+        main()
 
