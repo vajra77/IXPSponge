544184f18f1483e53827ba6217fe752a50df85e7
vajra77 <f.ferreri@namex.it>
2025-06-26 15:08:53 +0200

    changed config switch

--- xsponget.py
+++ xsponget.py
@@ -31,9 +31,9 @@ if __name__ == "__main__":
     opts, args = getopt.getopt(sys.argv[1:], "hc:", ["help", "config="])
 
     for opt, arg in opts:
-        if opt == "--config":
+        if opt == "-c":
             config_file = arg
-        elif opt == "--help":
+        elif opt == "-h":
             usage()
             exit(0)
         else:
