2b15823e1e3a91f9847cd89187d2294263cd076a
vajra77 <f.ferreri@namex.it>
2025-07-15 11:30:00 +0200

    added xstorm

--- xstorm.py
+++ xstorm.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
 
     while True:
         address = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
-        request = ARP(op=2, hwsrc=src_mac, psrc=src_addr, hwdst="ff:ff:ff:ff:ff:ff", pdst=address)
+        # request = ARP(op=2, hwsrc=src_mac, psrc=src_addr, hwdst="ff:ff:ff:ff:ff:ff", pdst=address)
+        request = ARP(pdst=address)
         frame = Ether(dst="ff:ff:ff:ff:ff:ff", src=src_mac) / request
         sendp(frame, iface=interface)
