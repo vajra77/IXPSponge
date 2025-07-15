49a686348b5263b49921724500cd7df48d9725c9
vajra77 <f.ferreri@namex.it>
2025-07-15 10:56:39 +0200

    added xstorm

---
+++ xstorm.py
@@ -0,0 +1,19 @@
+import sys
+import random
+import struct
+import socket
+from scapy.layers.l2 import ARP, Ether
+from scapy.sendrecv import sendp
+from scapy.arch import get_if_hwaddr
+
+
+if __name__ == '__main__':
+    interface = sys.argv[1]
+    src_addr = sys.argv[2]
+    src_mac = get_if_hwaddr(interface)
+
+    while True:
+        address = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+        request = ARP(op=2, hwsrc=src_mac, psrc=src_addr, hwdst="ff:ff:ff:ff:ff:ff", pdst=address)
+        frame = Ether(dst="ff:ff:ff:ff:ff:ff", src=src_mac) / request
+        sendp(frame, iface=interface)
