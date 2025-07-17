e5e0c0c6d14d3f1d3ddffb5f20ae0dd622891203
fferreri <f.ferreri@namex.it>
2025-07-17 15:41:14 +0200

    added dhcp broadcast

--- xstorm.py
+++ xstorm.py
@@ -3,8 +3,11 @@ import random
 import struct
 import socket
 from scapy.layers.l2 import ARP, Ether
+from scapy.layers.dhcp import DHCP, BOOTP
+from scapy.layers.inet import IP, UDP
 from scapy.sendrecv import sendp
 from scapy.arch import get_if_hwaddr, get_if_addr
+from scapy.arch.unix import get_if_raw_hwaddr
 
 
 if __name__ == '__main__':
@@ -15,6 +18,15 @@ if __name__ == '__main__':
     while True:
         address = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
         # request = ARP(op=2, hwsrc=src_mac, psrc=src_addr, hwdst="ff:ff:ff:ff:ff:ff", pdst=address)
-        request = ARP(pdst=address)
-        frame = Ether(dst="ff:ff:ff:ff:ff:ff", src=src_mac) / request
+        # request = ARP(pdst=address, psrc="192.168.204.1")
+        # frame = Ether(dst="ff:ff:ff:ff:ff:ff", src=src_mac) / request
+
+        ethernet = Ether(dst='ff:ff:ff:ff:ff:ff', src=src_mac, type=0x800)
+        ip = IP(src=src_addr, dst='255.255.255.255')
+        udp = UDP(sport=68, dport=67)
+        #fam, hw = get_if_raw_hwaddr(interface)
+        bootp = BOOTP(chaddr=src_mac, ciaddr='0.0.0.0', xid=0x01020304, flags=1)
+        dhcp = DHCP(options=[("message-type", "discover"), "end"])
+        frame = ethernet / ip / udp / bootp / dhcp
+
         sendp(frame, iface=interface)
